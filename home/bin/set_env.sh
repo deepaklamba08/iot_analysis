6c5809768de35b178dd1c6340ad55cd856385d87
deepaklamba08 <deepaklamba.08@gmail.com>
2024-12-17 16:32:59 +0530

    Updated app

--- home/bin/set_env.sh
+++ home/bin/set_env.sh
@@ -1,10 +1,11 @@
-export PYTHON_HOME="/e/work/python_env/Scripts/python.exe"
-export PYTHON_ANALYSIS_APP_NAME="/e/work/iot_analysis/src/analysis_app.py"
 export PATH_TO_ANALYSIS_APP="/e/work/iot_analysis/"
-export DEFAULT_ARGS_TO_ANALYSIS_APP="execution_summary_dir /e/work/iot_analysis/test/summary"
+export PYTHON_HOME="/e/work/python_env/Scripts/python.exe"
 
-export PYTHON_WEB_APP_NAME="/e/work/iot_analysis/web_app/app.py"
-export PATH_TO_WEB_APP="/e/work/iot_analysis/"
-export DEFAULT_ARGS_TO_WEB_APP="execution_summary_dir /e/work/iot_analysis/test/summary"
+export PYTHON_ANALYSIS_APP_NAME="${PATH_TO_ANALYSIS_APP}/src/analysis_app.py"
+export DEFAULT_ARGS_TO_ANALYSIS_APP="execution_summary_dir ${PATH_TO_ANALYSIS_APP}/test/summary"
 
-export PYTHON_ANALYSIS_JOB_EXECUTOR_APP_NAME="/e/work/iot_analysis/src/job_executor.py"
+export PYTHON_WEB_APP_NAME="${PATH_TO_ANALYSIS_APP}/web_app/app.py"
+export PATH_TO_WEB_APP="${PATH_TO_ANALYSIS_APP}/"
+export DEFAULT_ARGS_TO_WEB_APP="execution_summary_dir ${PATH_TO_ANALYSIS_APP}/test/summary"
+
+export PYTHON_ANALYSIS_JOB_EXECUTOR_APP_NAME="${PATH_TO_ANALYSIS_APP}/src/job_executor.py"
