292a476d6e66916791f7abc27d39fa8f778d9140
Deepak <deepaklamba.08@gmail.com>
2024-12-20 21:02:44 +0530

    shell updated

--- home/bin/run_webapp.sh
+++ home/bin/run_webapp.sh
@@ -155,7 +155,8 @@ run_app(){
 
   log_message "INFO" "Shell cmd - $SHELL_CMD"
 
-  eval $SHELL_CMD
+  #eval $SHELL_CMD
+  nohpu $SHELL_CMD &
   APP_RUN_STATUS=$?
   $APP_RUN_STATUS > $PID_FILE
   log_message "INFO" "App run status code - $APP_RUN_STATUS"
