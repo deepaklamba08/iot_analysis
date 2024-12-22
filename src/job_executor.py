f042bc0c2893c164c0faef5e1212536759b4c150
Deepak <deepaklamba.08@gmail.com>
2024-12-22 07:20:20 +0530

    Job executor updated

--- src/job_executor.py
+++ src/job_executor.py
@@ -37,7 +37,7 @@ class JobExecutor:
                 runtime_context = self.create_runtime_context(scheduled_job.parameters)
                 application_store = ApplicationStore(runtime_context.config_file(), runtime_context.parameters)
                 application = application_store.lookup_application(scheduled_job.app_id)
-                self.logger.debug(f'loaded application - f{application.name}')
+                self.logger.debug(f'loaded application - {application.name}')
                 if not application:
                     self.logger.error(f'application not found by id - {scheduled_job.app_id}')
                     raise Exception(f'application not found by id - {scheduled_job.app_id}')
