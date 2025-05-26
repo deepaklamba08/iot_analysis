d1d95658f2305235eecc1dac49a535937e168275
deepaklamba08 <deepaklamba.08@gmail.com>
2025-05-26 18:23:56 +0530

    Scheduler integration

--- src/utils.py
+++ src/utils.py
@@ -87,7 +87,7 @@ def read_config_file(config_file_path: str):
     return yaml_config
 
 
-def get_env_config(key:str):
+def get_env_config(key: str):
     env_config = os.environ.get(key)
     if not env_config:
         raise Exception(f'key not found in environment - {key}')
