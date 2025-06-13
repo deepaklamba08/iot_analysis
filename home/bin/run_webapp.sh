440ad514920526655a35b8ef9ca1322499468a15
deepaklamba08 <deepaklamba.08@gmail.com>
2025-06-13 17:05:09 +0530

    Shell updated

--- home/bin/run_webapp.sh
+++ home/bin/run_webapp.sh
@@ -157,7 +157,7 @@ run_app(){
 
   log_message "INFO" "Shell cmd - $SHELL_CMD"
 
-  eval $SHELL_CMD &
+  eval $SHELL_CMD
   APP_RUN_STATUS=$?
   $APP_RUN_STATUS > $PID_FILE
   log_message "INFO" "App run status code - $APP_RUN_STATUS"
