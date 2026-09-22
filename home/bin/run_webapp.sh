2d8bed9c418885d566ea9abb3cb1bbf620f01e45
deepaklamba08 <deepaklamba.08@gmail.com>
2026-09-22 11:50:30 +0530

    wrapper script updated

--- home/bin/run_webapp.sh
+++ home/bin/run_webapp.sh
@@ -263,7 +263,7 @@ run_app(){
 
   CLI_INPUT_STRING="['config_file','$CONFIG_FILE_PATH', 'submitter', '$SUBMITTER']" # ${GENERIC_PARAMETERS[@]} $DEFAULT_ARGS_TO_WEB_APP"
   log_message "INFO" "CLI input - $CLI_INPUT_STRING"
-  SHELL_CMD="$PYTHON_HOME -m flask --app \"$PYTHON_WEB_APP_NAME:create_app($CLI_INPUT_STRING)\" run --host $HOST_NAME --debug"
+  SHELL_CMD="$PYTHON_HOME -m flask --app \"$PYTHON_WEB_APP_NAME:create_app($CLI_INPUT_STRING)\" run --host $HOST_NAME"
 
   log_message "INFO" "Shell cmd - $SHELL_CMD"
 
